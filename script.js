6eee3bb76997665dcf2b835aa17f27a528320ede
Nate Schroeder <n8dogg59@hotmail.com>
2021-01-09 16:40:39 -0600

    mvp

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
-
-
-
 $(document).ready(function() {
+    // display the current day in the format required
     $("#currentDay").text(moment().format('dddd MMMM Do, YYYY'));
 
+    // get the current hour so that we can use this number to change the task descriptions background color
     var currentHour = moment().format("HH");
     console.log("current hour is " + currentHour);
 
+    // creates an array for the input elements and stores them in variable hourArr
     var hourArr = $("input")
+
+    // for loop that checks the current time again the hour row and sets the color accordingly
     for (var i = 0; i < hourArr.length; i++) {
         var timeId = $(hourArr[i]).attr("id");
 
         if (currentHour > timeId) {
-            $(hourArr[i]).addClass("bg-secondary")
+            $(hourArr[i]).addClass("bg-secondary text-light")
         }
         else if (currentHour === timeId) {
             $(hourArr[i]).addClass("bg-danger")
         }         
     }
 
+    // function funs every second to see if the hour has changed to set the background color
+    var checkTime = setInterval(function() {
+        for (var i = 0; i < hourArr.length; i++) {
+            var timeId = $(hourArr[i]).attr("id");
+
+            if (currentHour > timeId) {
+                $(hourArr[i]).addClass("bg-secondary text-light")
+            }
+            else if (currentHour === timeId) {
+                $(hourArr[i]).addClass("bg-danger")
+            }         
+        }
+    }, 1000);
+
+    // when save button is clicked this function sends the key and value to local storage
     $(".saveBtn").on("click", function() {
         console.log($(this).siblings(".col-10").val());
         var timeSave = $(this).siblings("input").attr("id");
@@ -28,6 +45,7 @@ $(document).ready(function() {
         localStorage.setItem(timeSave, taskSave)
     })
 
+    // when page is reloaded all the task descriptions in local storage get loaded
     $("#09").val(localStorage.getItem("09"));
     $("#10").val(localStorage.getItem("10"));
     $("#11").val(localStorage.getItem("11"));
